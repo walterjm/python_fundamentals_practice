4a79e2c6932939515627d0f514de1de50fb55ae3
walterjm <walterjm@yahoo.com>
2017-07-03 14:39:50 -0700

    Add files via upload

---
+++ multiples_sum_average.py
@@ -0,0 +1,22 @@
+# Part I - Write code that prints all the odd numbers from 1 to 1000. Use the for loop and don't use a list to do this exercise.
+# for count in range (1,1001):
+#     if count % 2 == 1:
+#         print count
+
+
+#this will print multiples of 5 from 5 to 1 million
+# for count in range (5,1000001):
+#     if count % 5 == 0:
+#         print count
+
+
+#this will print the sum of all the values in the list
+# a = [1,2,5,10,255,3]
+# sum_of_numbers = sum(a)
+# print sum_of_numbers
+
+
+#this will give me the average of the numbers in the list
+# a = [1,2,5,10,255,3]
+# average_of_numbers = sum(a)
+# print "Average of numbers is", average_of_numbers / len(a)
